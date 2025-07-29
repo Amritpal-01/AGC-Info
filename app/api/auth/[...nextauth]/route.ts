ecc1523c537ed85fb4ebc262fc4d38ae95c1ed9d
Amritpal-01 <Amritpalonly13571@gmail.com>
2025-07-29 10:04:44 +0530

    .

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -26,7 +26,7 @@ const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.NEXT_PUBLIC_CLIENT_ID as string,
-      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET as string,
+      clientSecret: process.env.NEXTAUTH_SECRET as string,
     }),
   ],
   callbacks: {
