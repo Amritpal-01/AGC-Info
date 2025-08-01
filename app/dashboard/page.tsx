e6a7b4c7ffb8d92b617a496dc29a66b534d5feab
Amritpal-01 <amritpalonly13571@gmail.com>
2025-08-01 15:59:37 +0530

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -108,7 +108,7 @@ const Page: React.FC = () => {
                   return <Collection key={collection.id}  collection={collection} path={"dashboard"}/>
                 })}
               </div>
-                <div className='w-full flex gap-4 justify-around'>
+                <div className='w-full flex flex-col gap-4 justify-around'>
                   <div className="text-center flex-1/2">
                   <motion.button whileTap={{ scale: 0.95 }} onClick={() => { redirect("/collections") }} className="inline-flex w-full items-center px-5 py-2 border border-transparent text-sm font-medium rounded-full text-red-600 bg-red-100 hover:bg-red-200 ">
                     <Eye className="w-5 h-5 mr-2" />
