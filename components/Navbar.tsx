ad75ed744b1d4cd74237213eee78e1479ad7d529
Amritpal-01 <Amritpalonly13571@gmail.com>
2025-08-03 15:57:28 +0530

    .

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,6 +15,7 @@ const Navbar: React.FC = () => {
   const { session } = useAuth() as AuthContextType
   const pathname = usePathname();
   const [showProfileOptions, setShowProfileOptions] = useState<boolean>(false)
+  const [menuToggle, setMenuToggle] = useState<boolean>(false)
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
@@ -59,7 +60,7 @@ const Navbar: React.FC = () => {
 
       </div>
 
-      <div className='flex gap-3 relative items-center min-w-10 min-h-10'>
+      <div className='z-50 flex gap-3 relative items-center min-w-10 min-h-10'>
         {pathname === "/dashboard" && <><motion.button initial={{scale: 0}} animate={{scale:1}} onClick={() => {redirect("/collections")}} className='absolute px-2 max-[600px]:hidden'>
           <SearchIcon className='text-blue-700'/>
         </motion.button>
@@ -67,7 +68,10 @@ const Navbar: React.FC = () => {
         {pathname === "/dashboard" && <motion.button initial={{scale: 0}} animate={{scale:1}} whileTap={{ scale: 0.95 }} onClick={() => { redirect("/collections") }} className={`min-[600px]:hidden text-blue-700 bg-blue-100 border border-blue-300 w-10 h-10 rounded-xl flex items-center justify-center`}><SearchIcon /></motion.button>}
         {pathname !== "/dashboard" && <motion.button initial={{scale: 0}} animate={{scale:1}} whileTap={{ scale: 0.95 }} onClick={() => { redirect("/dashboard") }} className={`text-green-700 bg-green-100 border border-green-300 w-10 h-10 rounded-xl flex items-center justify-center`}><DraftingCompassIcon /></motion.button>}
         <motion.button initial={{scale: 0}} animate={{scale:1}} whileTap={{ scale: 0.95 }} onClick={() => { redirect("/") }} className={`text-green-700 bg-green-100 border border-green-300 w-10 h-10 rounded-xl flex items-center justify-center`}><InfoIcon /></motion.button>
-        <motion.button initial={{scale: 0}} animate={{scale:1}} whileTap={{ scale: 0.95 }} className={`text-black-700 bg-black-100 border border-black-300 w-10 h-10 rounded-xl flex items-center justify-center`}><MenuIcon /></motion.button>
+        <div className='relative h-full flex items-center'>
+          <motion.button initial={{scale: 0}} animate={{scale:1}} whileTap={{ scale: 0.95 }} onClick={() => {setMenuToggle(!menuToggle)}} className={`text-black-700 bg-black-100 border border-black-300 w-10 h-10 rounded-xl flex items-center justify-center`}><MenuIcon /></motion.button>
+            <div className={`absolute top-full -right-4 h-96 ${menuToggle?"w-64  rounded-bl-xl shadow-gray-300 shadow-2xl":"w-0"} transition-all duration-300 overflow-hidden bg-gradient-to-b from-[#E8EEF2] to-white`}></div>
+        </div>
       </div>
     </div>
   )
